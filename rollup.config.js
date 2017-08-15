4cf20c57f77476dcf9435cd2db028e4e97037908
Luca Barone <baro.luc@gmail.com>
2017-08-15 12:35:26 +0200

    :construction: prepare rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+
+const minify = false
 
 export default {
   entry: 'source/index.js',
-  targets: [{dest: `dist/react-navs.cjs.js`, format: 'cjs'}],
-  external: ['react', 'prop-types'],
-  format: 'es',
+  targets: [{dest: `dist/react-navs.es.js`, format: 'es'}],
   exports: 'named',
   moduleName: 'ReactNavs',
-  plugins: [babel({exclude: 'node_modules/**'})].filter(Boolean),
+  format: 'es',
+  external: ['react', 'prop-types'],
+  plugins: [
+    nodeResolve({jsnext: true, main: true}),
+    commonjs({include: 'node_modules/**'}),
+    babel({exclude: 'node_modules/**'}),
+    minify && uglify(),
+  ].filter(Boolean),
 }
